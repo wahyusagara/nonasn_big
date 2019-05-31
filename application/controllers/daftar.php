e6af8d0e67294fb81e5f8cc6d504554b8b35a32a
wahyusagara <joblinggar@gmail.com>
2019-05-31 15:24:34 +0700

    sql

--- application/controllers/daftar.php
+++ application/controllers/daftar.php
@@ -23,44 +23,44 @@ class Daftar extends CI_Controller {
 	}
 	public function absen()
 	{
-		$curl = curl_init();
+		// $curl = curl_init();
 
-		curl_setopt_array($curl, array(
-		CURLOPT_URL => "http://10.10.160.189/sinkron/konek.php",
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
-		CURLOPT_HTTPHEADER => array(
-			"cache-control: no-cache",
-			"postman-token: a9eb45ca-e4bf-bc8b-803c-73f276fe4032"
-		),
-		));
+		// curl_setopt_array($curl, array(
+		// CURLOPT_URL => "http://10.10.160.189/sinkron/konek.php",
+		// CURLOPT_RETURNTRANSFER => true,
+		// CURLOPT_ENCODING => "",
+		// CURLOPT_MAXREDIRS => 10,
+		// CURLOPT_TIMEOUT => 30,
+		// CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		// CURLOPT_CUSTOMREQUEST => "GET",
+		// CURLOPT_HTTPHEADER => array(
+		// 	"cache-control: no-cache",
+		// 	"postman-token: a9eb45ca-e4bf-bc8b-803c-73f276fe4032"
+		// ),
+		// ));
 
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
+		// $response = curl_exec($curl);
+		// $err = curl_error($curl);
 
-		curl_close($curl);
+		// curl_close($curl);
 		
-		// print $response;
-		// die();
+		// // print $response;
+		// // die();
 
-		if ($err) {
-			echo "cURL Error #:" . $err;
-		} else {
+		// if ($err) {
+		// 	echo "cURL Error #:" . $err;
+		// } else {
 			
-			echo "<script>console.log('" . json_encode($response) . "');</script>";
+		// 	echo "<script>console.log('" . json_encode($response) . "');</script>";
 			
-		// echo "<pre>";
-		// echo $response;
+		// // echo "<pre>";
+		// // echo $response;
 		
-		}
+		// }
 		
 		
 		$this->load->view("part/nav");
-		$this->load->view("absen", $response);			
+		$this->load->view("absen");			
 		$this->load->view("part/footer");
 	}
 
--- application/views/absen.php
+++ application/views/absen.php
@@ -1,11 +1,36 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
+</script>
+
 <?php  
  $sumber = 'http://10.10.160.189/sinkron/konek.php';
-
- 
  $user = $_SESSION["user_name"];
- 
+//  echo $user;
  $konten = file_get_contents($sumber);
  $data = json_decode($konten, true);
+
+//  $json = '[{"name":"Juan","Sex":"Male","ID":"1100"},{"name":"Maria","Sex":"Female","ID":"2513"},{"name":"Pedro","Sex":"Male","ID":"2211"}]';
+  // $array = json_decode($data,1);
+  $ID = $user;
+  $list = array_filter($data, function ($var) use ($ID) {
+      return ($var['userID'] == $ID);
+  });
+  // print_r($list);
 
 ?>
 
@@ -23,23 +48,26 @@
       </tr>
     </thead>
     <tbody>
-    <?php   
-      for($a=1; $a < count($data); $a++)
+    <?php 
+      for($a=1;$a < count($list);$a++)
+      // for($i=0;$i < count($item);$i++)
       {
+        
         print "<tr>";
         // penomeran otomatis
         print "<td>".$a."</td>";
         // menayangkan 
-        print "<td>".$data[$a]['userID'] = "1111"."</td>";
-        print "<td>".$data[$a]['terminalName']."</td>";
-        print "<td>".substr_replace( $data[$a][''], '', 10 )."</td>";
-        print "<td>".substr($data[$a][''], 10, -3)."</td>";
+        print "<td>".$list[$a]['userID']."</td>";
+        print "<td>".$list[$a]['terminalName']."</td>";
+        print "<td>".substr_replace( $list[$a][''], '', 10 )."</td>";
+        print "<td>".substr($list[$a][''], 10, -3)."</td>";
 
         // print "<td>".$data[$a]['']."</td>";
         // print substr_replace( $data[$a][''], ':', 2 );
         print "</tr>";
       }
     ?>
+    
       
     </tbody>
     <tfoot>
